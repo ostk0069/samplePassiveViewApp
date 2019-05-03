7dd9b5b74356ec9a583a1e928716e360a8db74e9
Takuma Osada <judgment9ttk069@gmail.com>
2019-05-03 15:01:03 +0900

    update SearchUserVC

--- samplePassiveViewApp/View/ViewController/SearchUserViewController.swift
+++ samplePassiveViewApp/View/ViewController/SearchUserViewController.swift
@@ -10,13 +10,71 @@ import UIKit
 
 class SearchUserViewController: UIViewController {
 
-  @IBOutlet weak var searchBar: UISearchBar!
-  @IBOutlet weak var tableView: UITableView!
-  
-  override func viewDidLoad() {
-    super.viewDidLoad()
-  }
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
 
+    private var presenter: SearchUserPresenterInput
 
+    func inject(presenter: SearchUserPresenterInput) {
+        self.presenter = presenter
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUp()
+    }
+    
+    private func setUp() {
+        tableView.estimatedRowHeight = 64
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.register(UINib(nibName: "UserCell", bundle: nil), forCellReuseIdentifier: "UserCell")
+    }
 }
 
+extension SearchUserViewController: UISearchBarDelegate {
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        presenter.didTapSearchButton(text: searchBar.text)
+    }
+}
+
+extension SearchUserViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView,
+                   didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        presenter.didSelectRow(at: indexPath)
+    }
+}
+
+extension SearchUserViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView,
+                   numberOfRowsInSection section: Int) -> Int {
+        return presenter.numberOfUsers
+    }
+    
+    func tableView(_ tableView: UITableView,
+                   cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "UserCell") as! UserCell
+        
+        if let user = presenter.user(forRow: indexPath.row) {
+            cell.configure(user: user)
+        }
+        return cell
+    }
+}
+
+extension SearchUserViewController: SearchUserPresenterOutput {
+    
+    func updateUsers(_ users: [User]) {
+        tableView.reloadData()
+    }
+    
+    func transitionToUserDetail(userName: String) {
+        let userDetailVC = UIStoryboard(
+            name: "UserDetail",
+            bundle: nil
+            ).instantiateInitialViewController()
+    }
+}
